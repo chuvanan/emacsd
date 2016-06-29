9ee56fccc18f2639f521e163ae214091e30166e0
An Chu <chuvanan.cva@gmail.com>
2016-06-29 16:31:10 +0700

    add jump to mark function

--- init.el
+++ init.el
@@ -118,6 +118,11 @@
 (require 'use-package)
 (setq use-package-verbose t)
 
+(use-package visible-mark
+  :ensure t
+  :config
+  (global-visible-mark-mode 1))
+
 (use-package smart-mode-line
   :ensure t
   :config (progn
@@ -300,7 +305,7 @@
      (ess-R-fl-keyword:F&T))))
  '(package-selected-packages
    (quote
-    (company-jedi avy imenu-anywhere aggressive-indent zenburn-theme projectile powerline base16-theme tango-plus-theme greymatters-theme flatui-theme meaculpa-theme smart-mode-line csv-mode helm-R helm which-key smex evil window-numbering company easy-kill use-package magit solarized-theme expand-region markdown-mode auto-complete smartparens org)))
+    (icicles visible-mark company-jedi avy imenu-anywhere aggressive-indent zenburn-theme projectile powerline base16-theme tango-plus-theme greymatters-theme flatui-theme meaculpa-theme smart-mode-line csv-mode helm-R helm which-key smex evil window-numbering company easy-kill use-package magit solarized-theme expand-region markdown-mode auto-complete smartparens org)))
  '(send-mail-function (quote mailclient-send-it))
  '(show-paren-mode t)
  '(size-indication-mode t)
@@ -356,3 +361,19 @@
         (ess-show-buffer (buffer-name sbuffer) nil)))))
 
 (define-key polymode-mode-map "\M-ns" 'ess-rmarkdown)
+
+;; (defun push-mark-no-activate ()
+;;   "Pushes `point' to `mark-ring' and does not activate the region
+;;    Equivalent to \\[set-mark-command] when \\[transient-mark-mode] is disabled"
+;;   (interactive)
+;;   (push-mark (point) t nil)
+;;   (message "Pushed mark to ring"))
+
+(global-set-key (kbd "C-`") 'push-mark-no-activate)
+
+(defun jump-to-mark ()
+  "Jumps to the local mark, respecting the `mark-ring' order.
+  This is the same as using \\[set-mark-command] with the prefix argument."
+  (interactive)
+  (set-mark-command 1))
+(global-set-key (kbd "C-j") 'jump-to-mark)
