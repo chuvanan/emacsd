275180139baa740922c3f99258f7253a889dec22
An Chu <chuvanan.cva@gmail.com>
2016-06-30 22:58:41 +0700

    add shiny shortcut

--- init.el
+++ init.el
@@ -365,6 +365,29 @@
 
 (define-key polymode-mode-map "\M-ns" 'ess-rmarkdown)
 
+(defun ess-rshiny ()
+  "Compile R markdown (.Rmd). Should work for any output type."
+  (interactive)
+  ;; Check if attached R-session
+  (condition-case nil
+      (ess-get-process)
+    (error
+     (ess-switch-process)))
+  (let* ((rmd-buf (current-buffer)))
+    (save-excursion
+      (let* ((sprocess (ess-get-process ess-current-process-name))
+             (sbuffer (process-buffer sprocess))
+             (buf-coding (symbol-name buffer-file-coding-system))
+             (R-cmd
+              (format "library(rmarkdown); rmarkdown::run(\"%s\")"
+                      buffer-file-name)))
+        (message "Running shiny on %s" buffer-file-name)
+        (ess-execute R-cmd 'buffer nil nil)
+        (switch-to-buffer rmd-buf)
+        (ess-show-buffer (buffer-name sbuffer) nil)))))
+
+(define-key polymode-mode-map "\M-nr" 'ess-rshiny)
+
 ;; (defun push-mark-no-activate ()
 ;;   "Pushes `point' to `mark-ring' and does not activate the region
 ;;    Equivalent to \\[set-mark-command] when \\[transient-mark-mode] is disabled"
