8b4c17de57462150e27393c646f55123834eef0c
An Chu <chuvanan.cva@gmail.com>
2016-07-03 22:17:19 +0700

    add silver searcher

--- init.el
+++ init.el
@@ -125,6 +125,13 @@
 (require 'use-package)
 (setq use-package-verbose t)
 
+;; silver searcher
+(use-package ag
+  :ensure t
+  :config
+  :bind (("<f3>" . ag-regexp)
+         ("<f4>" . ag-dired-regexp)))
+
 ;; markdown mode
 (use-package markdown-mode
   :ensure t
@@ -336,7 +343,7 @@
      (ess-R-fl-keyword:F&T))))
  '(package-selected-packages
    (quote
-    (ido-ubiquitous ace-window evil-leader keyfreq apropospriate-theme seoul256-theme icicles visible-mark company-jedi avy imenu-anywhere aggressive-indent zenburn-theme projectile powerline base16-theme tango-plus-theme greymatters-theme flatui-theme meaculpa-theme smart-mode-line csv-mode helm-R helm which-key smex evil window-numbering company easy-kill use-package magit solarized-theme expand-region markdown-mode auto-complete smartparens org)))
+    (ag ido-ubiquitous ace-window evil-leader keyfreq apropospriate-theme seoul256-theme icicles visible-mark company-jedi avy imenu-anywhere aggressive-indent zenburn-theme projectile powerline base16-theme tango-plus-theme greymatters-theme flatui-theme meaculpa-theme smart-mode-line csv-mode helm-R helm which-key smex evil window-numbering company easy-kill use-package magit solarized-theme expand-region markdown-mode auto-complete smartparens org)))
  '(send-mail-function (quote mailclient-send-it))
  '(show-paren-mode t)
  '(size-indication-mode t)
