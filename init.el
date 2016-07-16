f88bcee3235c03589f2409e51823350ef546817d
An Chu <chuvanan.cva@gmail.com>
2016-07-16 17:48:48 +0700

    add rename-file-and-buffer

--- init.el
+++ init.el
@@ -746,3 +746,18 @@ and the point, not include the isearch word."
   (find-file-other-window user-init-file))
 
 (global-set-key (kbd "C-c I") 'find-user-init-file)
+
+(defun rename-file-and-buffer ()
+  "Rename the current buffer and file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+          (set-visited-file-name new-name t t)))))))
+
+(global-set-key (kbd "C-c r")  'rename-file-and-buffer)
